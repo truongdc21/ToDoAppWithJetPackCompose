7af38adc84eaa89256740d1b99bd5b65ca24c2bf
truongdc-3155 <dang.chi.truong@sun-asterisk.com>
2023-06-08 18:36:21 +0700

    Basic logic at Todo list screen

--- app/src/main/java/com/truongdc21/todoappwithjetpackcompose/ToDoListScreen.kt
+++ app/src/main/java/com/truongdc21/todoappwithjetpackcompose/ToDoListScreen.kt
@@ -1,21 +1,44 @@
 package com.truongdc21.todoappwithjetpackcompose
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.material.Button
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Checkbox
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.truongdc21.todoappwithjetpackcompose.model.ToDo
 
+@SuppressLint("MutableCollectionMutableState")
 @Composable
 fun ToDoListScreen(navController: NavHostController) {
     Column(
@@ -25,12 +48,117 @@ fun ToDoListScreen(navController: NavHostController) {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        Text(text = "To Do List screen ", fontSize = 26.sp, color = Color.White)
-        Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = {
+        DefaultScreen(navController = navController)
+    }
+}
+
+@Composable
+private fun DefaultScreen(
+    navController: NavHostController,
+) {
+    val todoList = remember {
+        mutableStateListOf(
+            ToDo(1, "Task 1", true),
+            ToDo(2, "Task 2", false),
+            ToDo(3, "Task 3", true)
+        )
+    }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+    ) {
+        StatusBar {
             navController.navigate(Constant.ADD_TODO_SCREEN)
-        }) {
-            Text(text = "Navigate To Add Todo Screen..")
         }
+        LazyColumn() {
+            items(todoList) { todo ->
+                ItemToDo(todo = todo, onTapChecked = {
+                    val index = todoList.indexOf(todo)
+                    todoList[index] = ToDo(todo.id, todo.title, it)
+                })
+            }
+
+        }
+    }
+}
+
+@Composable
+private fun StatusBar(onClickAddToDo: () -> Unit) {
+    Column {
+        Row(
+            Modifier
+                .background(Color.White)
+                .padding(10.dp)
+        ) {
+            Text(
+                text = stringResource(id = R.string.to_do_list),
+                modifier = Modifier
+                    .weight(1f)
+                    .align(Alignment.CenterVertically),
+                textAlign = TextAlign.Center,
+                fontSize = 21.sp,
+                fontWeight = FontWeight.Bold,
+                fontFamily = FontFamily.Monospace
+            )
+
+            ImgAddTodo(
+                modifier = Modifier
+                    .clickable(onClick = onClickAddToDo)
+                    .height(30.dp)
+                    .width(30.dp)
+                    .align(Alignment.CenterVertically)
+            )
+        }
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.DarkGray)
+                .height(1.dp)
+        )
+    }
+}
+
+@Composable
+private fun ImgAddTodo(modifier: Modifier) {
+    Image(
+        imageVector = Icons.Filled.Add,
+        contentDescription = null,
+        modifier = modifier
+    )
+}
+
+@Composable
+private fun ItemToDo(todo: ToDo, onTapChecked: (Boolean) -> Unit) {
+    Row(
+        modifier = Modifier
+            .padding(10.dp)
+            .fillMaxWidth()
+            .background(Color.White)
+            .border(1.dp, Color.DarkGray, shape = RoundedCornerShape(8.dp))
+            .padding(10.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = todo.title,
+            color = Color.Black,
+            modifier = Modifier.weight(1f)
+        )
+        Checkbox(checked = todo.isCompleted, onCheckedChange = { isChecked ->
+            onTapChecked(isChecked)
+        }, Modifier.align(Alignment.Top))
+    }
+}
+
+
+@Composable
+@Preview(
+    showSystemUi = true,
+    showBackground = true
+)
+private fun PreViewToDoListScreen() {
+    val navHostController = rememberNavController()
+    Column() {
+        DefaultScreen(navHostController)
     }
 }
